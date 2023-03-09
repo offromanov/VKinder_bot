197ae48fdac5e1f0031dfbe1f0f81eb6302152dd
offromanov <127434093+offromanov@users.noreply.github.com>
2023-03-09 21:23:43 +0300

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,3 @@
+token = 'vk1.a.0K8MzUWDUnjalFGEPtuDXDftohwrBmA9bMlQnPbDFCV_uodIEBfvM0XyuiuGoIXeXTPv9cOQTClsFXM3eapvM9ZiKig8u38pT_4aEgrvYJsUdk-plU4_130Id539YhNehYEvcoGAlLc7Y1FRWWSGkAul3bb3KH2sIhMyfBWTAuLLFqZ-KxJdqwDMrhG-Z87pusE5skeO6KhRdsSftSNY2Q'
+
+user_token = ''

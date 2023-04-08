41867c9c984c2f4e8113fb55ad3bbbdcffd3de29
offromanov <127434093+offromanov@users.noreply.github.com>
2023-04-08 19:18:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -177,38 +177,38 @@ if __name__ == '__main__':
 
             if request == 'поиск':
                 if info_for_search:
-                    '''здесь дублирование'''
-                    users = tools.users_search(info_for_search)
-                    user = users.pop()
-                    photos = tools.photos_get(user['id'])
-                    if photos:
-                        write_msg(vk_interface, user_id,
+                    def search_profile (self, user_id):
+                        users = tools.users_search(info_for_search)
+                        user = users.pop()
+                        photos = tools.photos_get(user['id'])
+                            if photos:
+                                write_msg(vk_interface, user_id,
                                   f'Страница пользователя {user["first_name"]} {user["last_name"]}')
-                        write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
-                        for photo in photos:
-                            write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
-                            add_data_users(user_id)
-                    else:
-                        write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
+                                write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
+                                for photo in photos:
+                                    write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
+                                    add_data_users(user_id)
+                            else:
+                                write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
 
 
                 else:
-                    '''здесь дублирование'''
-                    user_info = tools.get_profile_info(user_id)
-                    info_for_search = user_validator(user_info)
-                    users = tools.users_search(info_for_search)
-                    user = users.pop()
-                    photos = tools.photos_get(user['id'])
-                    if photos:
-                        write_msg(vk_interface, user_id,
+                    def get_photos (seif, user_id):
+                        user_info = tools.get_profile_info(user_id)
+                        info_for_search = user_validator(user_info)
+                        users = tools.users_search(info_for_search)
+                        user = users.pop()
+                        photos = tools.photos_get(user['id'])
+                            if photos:
+                                write_msg(vk_interface, user_id,
                                   f'Страница пользователя {user["first_name"]} {user["last_name"]}'
                                   )
-                        write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
-                        for photo in photos:
-                            write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
-                            add_data_users(user_id)
-                    else:
-                        write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
+                                write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
+                                for photo in photos:
+                                    write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
+                                    add_data_users(user_id)
+                                else:
+                                    write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
 
             if request == 'дальше':
                 user = users.pop()

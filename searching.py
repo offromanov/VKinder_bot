1824099eba5cfa14bb97bf91fea6b44923235bdf
offromanov <127434093+offromanov@users.noreply.github.com>
2023-05-12 22:30:58 +0300

    Add files via upload

--- searching.py
+++ searching.py
@@ -16,33 +16,33 @@ class Searching():
         info_for_search = None
         offset = 0
 
-            def search_profile(self, user_id, vk_interface, info_for_search):
-                users = self.users_search(info_for_search)
-                user = users.pop()
-                photos = self.photos_get(user['id'])
-                if photos:
-                    write_msg(vk_interface, user_id,
-                        f'Страница пользователя {user["first_name"]} {user["last_name"]}')
-                    write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
-                    for photo in photos:
-                        write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
-                        add_data_users(user_id)
+        def search_profile(self, user_id, vk_interface, info_for_search):
+            users = self.users_search(info_for_search)
+            user = users.pop()
+            photos = self.photos_get(user['id'])
+            if photos:
+                write_msg(vk_interface, user_id,
+                    f'Страница пользователя {user["first_name"]} {user["last_name"]}')
+                write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
+                for photo in photos:
+                    write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
+                    add_data_users(user_id)
                 else:
                     write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
 
-            def get_photos(self, user_id, vk_interface):
-                user_info = self.get_profile_info(user_id)
-                info_for_search = user_validator(user_info)
-                users = self.users_search(info_for_search)
-                user = users.pop()
-                photos = self.photos_get(user['id'])
-                if photos:
-                    write_msg(vk_interface, user_id,
-                        f'Страница пользователя {user["first_name"]} {user["last_name"]}'
-                        )
-                    write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
-                    for photo in photos:
-                        write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
-                        add_data_users(user_id)
-                    else:
-                        write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
+        def get_photos(self, user_id, vk_interface):
+            user_info = self.get_profile_info(user_id)
+            info_for_search = user_validator(user_info)
+            users = self.users_search(info_for_search)
+            user = users.pop()
+            photos = self.photos_get(user['id'])
+            if photos:
+                write_msg(vk_interface, user_id,
+                    f'Страница пользователя {user["first_name"]} {user["last_name"]}'
+                    )
+                write_msg(vk_interface, user_id, f'Ссылка на страницу vk.com/id{user["id"]}')
+                for photo in photos:
+                    write_msg(vk_interface, user_id, attachment=f'photo{photo["owner_id"]}_{photo["media_id"]}')
+                    add_data_users(user_id)
+                else:
+                    write_msg(vk_interface, user_id, 'внутренняя ошибка, приносим свои извинения')
